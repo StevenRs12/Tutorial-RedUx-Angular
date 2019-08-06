10e461dbf54f026e6100db25a92f7a5767026003
Steven Rojas <Steven8Rojas@gmail.com>
2019-08-06 11:37:09 -0500

    v1.1

--- src/app/todo/todos-list/todos-list.component.ts
+++ src/app/todo/todos-list/todos-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AppState } from 'src/app/app.reducers';
 import { Store } from '@ngrx/store';
 import { Todo } from '../model/todo.model';
+import * as fromFiltroActions from '../../filter/filter.action';
 
 @Component({
   selector: 'app-todos-list',
@@ -11,7 +12,7 @@ import { Todo } from '../model/todo.model';
 export class TodosListComponent implements OnInit {
 
   todos: Todo[] = [];
-  filtro: string;
+  filtro: fromFiltroActions.filtrosValidos;
 
   constructor(private store: Store<AppState>) { }
 
